5cd827fda8578eb6f6a56c7a09d958d761133949
Chris Buonocore <chrisdistrict@gmail.com>
2023-10-09 00:24:11 -0400

    removed unused, eslint check on build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
     images: {
         unoptimized: true, 
     },
+    eslint: {
+      // ignore eslint during build process
+      ignoreDuringBuilds: true,
+    },
   }
 
 module.exports = nextConfig
