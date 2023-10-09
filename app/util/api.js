f97b46d7980abbaee9dd7287078419919e221b38
Chris Buonocore <chrisdistrict@gmail.com>
2023-10-09 17:54:13 -0400

    node env

--- app/util/api.js
+++ app/util/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3000'
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://blockreach.vercel.app' : 'http://localhost:3000';
 
 const axiosInstance = axios.create({
     baseURL: baseUrl,
