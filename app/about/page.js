871e537fe3d579597faa2f3ed512042e02e8a23a
Chris Buonocore <chrisdistrict@gmail.com>
2023-10-11 12:24:45 -0400

    fix link

--- app/about/page.js
+++ app/about/page.js
@@ -25,7 +25,7 @@ export default function About() {
             {/* github */}
             <p>
                 {APP_NAME} is an open source project. You can find the code on GitHub here:&nbsp;
-                <a href="https://github.com/identity23" target="_blank">GitHub</a>&nbsp;
+                <a href="https://github.com/cbonoz/identity23" target="_blank">GitHub</a>&nbsp;
             </p>
 
             <p>Note this is a hackathon prototype and would require additional work to be mainnet ready. By uploading data you agree that this service is used as-is and that data may be compromised or shared outside the platform.</p>
